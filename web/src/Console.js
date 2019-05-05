7120687cc3abb5f13e9d2aa60d48756d2b7e54d4
jonas@valfridsson.net <jonas@valfridsson.net>
2019-05-05 19:14:03 +0200

    Make site reshape itself for small sites -- 'response'

--- web/src/Console.js
+++ web/src/Console.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import styled from 'styled-components'
-import AceEditor from "react-ace";
+import AceEditor from 'react-ace'
 
-import "brace/mode/json";
-import "brace/theme/clouds_midnight";
+import 'brace/mode/json'
+import 'brace/theme/clouds_midnight'
 
 const Con = styled.div`
   grid-area: console;
@@ -15,33 +15,33 @@ const request = `
 {
   "postnummer": 135551
 }
-`;
+`
 
 class Console extends React.Component {
-  constructor(props) {
+  constructor (props) {
     super(props)
 
-    this.monitor = (console.log);
-		this.text = request
+    this.monitor = (console.log)
+    this.text = request
   }
 
-  render() {
+  render () {
     return (
-        <Con className='console'>
-				 <AceEditor
-						ref={this.editorRef}
-						mode="json"
-						theme="clouds_midnight"
-						height="100%"
-						widht="100%"
-						value={this.text}
-						defaultValue={request}
-						onChange={request => this.text = request}
-						name="Console Editor"
-						editorProps={{ $blockScrolling: true }}
-					/>
-        </Con>
-      )
+      <Con className='console'>
+        <AceEditor
+          ref={this.editorRef}
+          mode='json'
+          theme='clouds_midnight'
+          height='100%'
+          widht='100%'
+          value={this.text}
+          defaultValue={request}
+          onChange={request => this.text = request}
+          name='Console Editor'
+          editorProps={{ $blockScrolling: true }}
+        />
+      </Con>
+    )
   }
 }
 
